c6c8087ce41f175f970bbee5a7e1aea563ac4063
SandeepKandagatla <kandagatlas@gmail.com>
2020-04-30 09:19:30 +0530

    fix

--- TweetBookAPI/Services/Identityservice.cs
+++ TweetBookAPI/Services/Identityservice.cs
@@ -124,7 +124,9 @@ namespace TweetBookAPI.Services
             var tokenHandler =new JwtSecurityTokenHandler();
             try
             {
-                var principal = tokenHandler.ValidateToken(token, _tokenValidationParameters,out var validatedToken);
+                var tokenValidationParameters = _tokenValidationParameters.Clone();
+                tokenValidationParameters.ValidateLifetime = false;
+                var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var validatedToken);
                 if (!IsJwtWithValidSecurityAlgorithm(validatedToken))
                 {
                     return null;
